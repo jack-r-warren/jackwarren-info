c7b2e91713f142550b8301888bd5e778767e7d5e
Jack Warren <jack@jackwarren.info>
2020-01-05 20:03:09 -0500

    Add netlify caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-netlify`,
+        "gatsby-plugin-netlify-cache",
         {
             resolve: `gatsby-transformer-remark`,
             options: {
